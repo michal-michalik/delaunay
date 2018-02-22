83055f8e01c2bbb87c3a8178ce1278b4c074fe1f
Michał <michalmichalik25@gmail.com>
2018-02-22 15:48:05 +0100

    Create Face test case

---
+++ tests/FaceTest.php
@@ -0,0 +1,21 @@
+<?php
+
+require_once __DIR__ . '/../src/Vector3.php';
+require_once __DIR__ . '/../src/Vertex.php';
+require_once __DIR__ . '/../src/HalfEdge.php';
+require_once __DIR__ . '/../src/Face.php';
+
+use PHPUnit\Framework\TestCase;
+
+class FaceTest extends TestCase
+{
+    public function testCreate()
+    {
+
+    }
+
+    public function testGetEdge()
+    {
+
+    }
+}
