1c10482e0765a49790ee259086f66d3778366a2f
Michał <michalmichalik25@gmail.com>
2018-02-26 13:22:32 +0100

    Create testHalfEdgeConnection

--- tests/FaceTest.php
+++ tests/FaceTest.php
@@ -9,13 +9,23 @@ use PHPUnit\Framework\TestCase;
 
 class FaceTest extends TestCase
 {
-    public function testCreate()
+    public function testHalfEdgeConnection()
     {
+        $vertex1 = new Vertex(0, 0, 0);
+        $vertex2 = new Vertex(5, 0, 0);
+        $vertex3 = new Vertex(0, 5, 0);
 
+        $face = new Face();
+        $face->create($vertex1, $vertex2, $vertex3);
+
+        $halfEdge1 = &$face->getEdge(0);
+        $halfEdge2 = &$halfEdge1->next->next->next;
+
+        $this->assertSame($halfEdge1, $halfEdge2);
+
+        $halfEdge2 = &$halfEdge1->prev->prev->prev;
+
+        $this->assertSame($halfEdge1, $halfEdge2);
     }
 
-    public function testGetEdge()
-    {
-
-    }
 }
